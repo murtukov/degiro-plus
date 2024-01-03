6f8a0838257e82dae7245c1b22c2151b493c6aa2
murtukov <murtukov@gmail.com>
2024-01-03 12:58:56 +0100

    Add new icons from TradingView

---
+++ icons/omx_icons.js
@@ -0,0 +1,17 @@
+const IMX_ICONS = {
+  "XACT_OMXS30": "xact-omxc25",
+  "XACTC25": "xact-omxc25",
+  "EFT1T": "eften-real-estate-fund-iii-share",
+  "SLG_OMXH25": "seligson-and-co-omx-helsinki-25",
+  "XACT_BULL_2": "xact-omxc25",
+  "XACTHDIV": "xact-omxc25",
+  "XACT_SMABOLAG": "xact-omxc25",
+  "XACT_BEAR_2": "xact-omxc25",
+  "XACT_SVERIGE": "xact-omxc25",
+  "XACT_BEAR": "xact-omxc25",
+  "XACT_BULL": "xact-omxc25",
+  "XACT_NORDEN": "xact-omxc25",
+  "XACT_OBLIGATION": "xact-omxc25",
+  "SSIGVEURR": "sparinvest-sa-global-value-r-eur-acc-nav",
+  "LYXGREENBOND": "amundi"
+}
